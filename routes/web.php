4ef6daa85e124d2a04df63b688b26d2dee89e2f2
Verga Tandika <verga.tandika@gmail.com>
2024-04-21 17:10:48 +0700

    initial commit

---
+++ routes/web.php
@@ -0,0 +1,18 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('pages.auth.auth-login');
+});
+
+Route::middleware(['auth'])->group(function () {
+    Route::get('/home', function () {
+        return view('pages.dashboard', ['type_menu' => 'home']);
+    })->name('home');
+});
+
+
+// Route::get('/login', function () {
+//     return view('pages.auth.auth-login');
+// });
