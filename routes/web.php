3eaf6f3611dfd65d75ffe7cc6755fabcba60a23d
Verga Tandika <verga.tandika@gmail.com>
2024-04-21 17:54:26 +0700

    crud user

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -10,6 +11,8 @@ Route::middleware(['auth'])->group(function () {
     Route::get('/home', function () {
         return view('pages.dashboard', ['type_menu' => 'home']);
     })->name('home');
+
+    Route::resource('users', UserController::class);
 });
 
 
